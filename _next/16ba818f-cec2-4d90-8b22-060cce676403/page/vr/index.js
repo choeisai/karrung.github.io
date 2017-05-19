2878c7a5d851ff3fd91405e46e528c746b19d7c1
Chalermrat Srinara <schalermrat@gmail.com>
2017-05-19 17:45:49 +0700

    Add reactbkk static web

---
+++ _next/16ba818f-cec2-4d90-8b22-060cce676403/page/vr/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/vr', function() {
+            var comp = module.exports=webpackJsonp([3],[],[232]);
+            return { page: comp.default }
+          })
+        

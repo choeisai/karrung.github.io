9d3e6b41dc97a410ada676b7a6eb27017e1d412e
Chalermrat Srinara <schalermrat@gmail.com>
2017-05-19 18:13:46 +0700

    Replace reactbkk 2.0.0 with React-VR

--- _next/16ba818f-cec2-4d90-8b22-060cce676403/page/vr/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/vr', function() {
-            var comp = module.exports=webpackJsonp([3],[],[232]);
-            return { page: comp.default }
-          })
-        

3cc525ac764dbe16d10d5fc950968d19b068a1a3
Chalermrat Srinara <schalermrat@gmail.com>
2017-05-20 19:37:22 +0700

    Update icon and hero image

--- _next/28fcd5db-82ea-458a-8cd6-62f5aa3364d2/page/vr/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/vr', function() {
-            var comp = module.exports=webpackJsonp([3],[],[237]);
-            return { page: comp.default }
-          })
-        
